1eccd46772326d37644da6bc7dd2d9bd6717c63d
Michael Winer <winer.mike@gmail.com>
2021-04-17 11:28:13 -0400

    consts?

---
+++ consts.js
@@ -0,0 +1,18 @@
+export const currencies = {
+  btc: {
+    quantity: 0.0939726 + 0.000903,
+  },
+  eth: {
+    quantity: 0.555517 + 0.03812381,
+  },
+  dot: {
+    quantity: 7.76006,
+  },
+  ada: {
+    quantity: 93.5173,
+  },
+};
+
+export const user = {
+  name: "Michael",
+}
