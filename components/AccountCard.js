9cb6763fa9f9bc72aec48837bcabdbd66348a70b
Michael Winer <winer.mike@gmail.com>
2021-04-17 17:14:35 -0400

    its perfect

--- components/AccountCard.js
+++ components/AccountCard.js
@@ -1,59 +1,46 @@
-import { useEffect, useState } from "react";
-import styles from "../styles/AccountCard.module.css";
-import { currencies } from "../const";
+import { useEffect, useState } from 'react'
+import styles from '../styles/AccountCard.module.css'
+import { currencies } from '../const'
 
 export default function AccountCard({ cryptoData, stockData, className }) {
-  const { btc, eth, ada, dot, vgro, other } = currencies;
-  const { BTC, ETH, ADA, DOT } = cryptoData?.crypto?.data || {};
+  const { btc, eth, ada, dot, vgro, other } = currencies
+  const { BTC, ETH, ADA, DOT } = cryptoData?.crypto?.data || {}
 
   // console.log(stockData?.stock['Global Quote']['08. previous close']);
-  const VGRORate = stockData?.stock["Global Quote"]["08. previous close"] || 0;
+  const VGRORate = stockData?.stock['Global Quote']['08. previous close'] || 0;
   // console.log("VGRO RATE:",VGRORate);
 
   const totaler = (currencies) => {
     if (cryptoData == undefined || BTC == undefined) {
-      return 0;
+      return 0
     }
     return Number(
       BTC.quote.CAD.price * btc.quantity +
         ETH.quote.CAD.price * eth.quantity +
         ADA.quote.CAD.price * ada.quantity +
         DOT.quote.CAD.price * dot.quantity
-    );
-  };
+    )
+  }
 
   return (
     <>
       <div className={className}>
         <h3 className="text-xl font-bold">Your Total Account Value:</h3>
-        <p>
-          BTC: {BTC && Number(BTC.quote.CAD.price * btc.quantity).toFixed(2)}
-        </p>
-        <p>
-          ETH: {ETH && Number(ETH.quote.CAD.price * eth.quantity).toFixed(2)}
-        </p>
-        <p>
-          ADA: {ADA && Number(ADA.quote.CAD.price * ada.quantity).toFixed(2)}
-        </p>
-        <p>
-          DOT: {DOT && Number(DOT.quote.CAD.price * dot.quantity).toFixed(2)}
-        </p>
-        <p>VGRO: {VGRORate && Number(VGRORate * vgro.quantity).toFixed(2)}</p>
-        <div>
-          <p className={styles.total}>
-            Crypto Total: ${totaler(currencies).toFixed(2)}
-          </p>
+        <p>BTC: ${BTC && Number(BTC.quote.CAD.price * btc.quantity).toFixed(2)}</p>
+        <p>ETH: ${ETH && Number(ETH.quote.CAD.price * eth.quantity).toFixed(2)}</p>
+        <p>ADA: ${ADA && Number(ADA.quote.CAD.price * ada.quantity).toFixed(2)}</p>
+        <p>DOT: ${DOT && Number(DOT.quote.CAD.price * dot.quantity).toFixed(2)}</p>
+        <p>VGRO: ${VGRORate && Number(VGRORate * vgro.quantity).toFixed(2)}</p>
+        <div className="bg-purple-100 py-2 px-3 rounded-xl text-purple-800 text-opacity-90">
+          <p className={styles.total}>Crypto Total: ${totaler(currencies).toFixed(2)}</p>
           <p className={styles.total}>
             Stock Total: ${Number(VGRORate * vgro.quantity).toFixed(2)}
           </p>
           <p className="font-bold text-md">
-            Portfolio Total: $
-            {(totaler(currencies) + Number(VGRORate * vgro.quantity)).toFixed(
-              2
-            )}
+            Portfolio Total: ${(totaler(currencies) + Number(VGRORate * vgro.quantity)).toFixed(2)}
           </p>
         </div>
       </div>
     </>
-  );
+  )
 }
--- pages/pie.js
+++ pages/pie.js
@@ -1,51 +1,51 @@
-import { useEffect, useState } from "react";
-import Head from "next/head";
-import AccountCard from "../components/AccountCard";
-import PieChart from "../components/PieComponent";
-import Pie2 from "../components/PieComponentRefactor";
-import styles from "../styles/Home.module.css";
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import AccountCard from '../components/AccountCard'
+import PieChart from '../components/PieComponent'
+import Pie2 from '../components/PieComponentRefactor'
+import styles from '../styles/Home.module.css'
 
-import { user, newPieData, currencies } from "../const";
+import { user, newPieData, currencies } from '../const'
 
 export default function Pie() {
-  const [cryptoData, setCryptoData] = useState(undefined);
-  const [stockData, setStockData] = useState(undefined);
-  const [cryptoTotal, setCryptoTotal] = useState(0);
-  const [stockTotal, setStockTotal] = useState(0);
+  const [cryptoData, setCryptoData] = useState(undefined)
+  const [stockData, setStockData] = useState(undefined)
+  const [cryptoTotal, setCryptoTotal] = useState(0)
+  const [stockTotal, setStockTotal] = useState(0)
 
-  const { btc, eth, ada, dot, vgro, other } = currencies;
-  const { BTC, ETH, ADA, DOT } = cryptoData?.crypto?.data || {};
+  const { btc, eth, ada, dot, vgro, other } = currencies
+  const { BTC, ETH, ADA, DOT } = cryptoData?.crypto?.data || {}
 
   // console.log(stockData?.stock['Global Quote']['08. previous close']);
-  const VGRORate = stockData?.stock["Global Quote"]["08. previous close"] || 0;
+  const VGRORate = stockData?.stock['Global Quote']['08. previous close'] || 0
   // console.log("VGRO RATE:",VGRORate);
 
   const totaler = (currencies) => {
     if (cryptoData == undefined || BTC == undefined) {
-      return 0;
+      return 0
     }
     return Number(
       BTC.quote.CAD.price * btc.quantity +
         ETH.quote.CAD.price * eth.quantity +
         ADA.quote.CAD.price * ada.quantity +
         DOT.quote.CAD.price * dot.quantity
-    );
-  };
+    )
+  }
 
   const getApiData = async () => {
-    let cryptoResults = await fetch("/api/crypto");
-    let cryptoData = await cryptoResults.json();
-    let stockResults = await fetch("/api/alphavantage");
-    let stockData = await stockResults.json();
-    setCryptoData(cryptoData);
-    setStockData(stockData);
-  };
+    let cryptoResults = await fetch('/api/crypto')
+    let cryptoData = await cryptoResults.json()
+    let stockResults = await fetch('/api/alphavantage')
+    let stockData = await stockResults.json()
+    setCryptoData(cryptoData)
+    setStockData(stockData)
+  }
 
   useEffect(async () => {
-    await getApiData();
+    await getApiData()
     // console.log("stock:",stockData);
     // console.log("crypto:",cryptoData);
-  }, []);
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -56,15 +56,24 @@ export default function Pie() {
 
       <div className="flex flex-row min-h-screen p-8 bg-gray-100">
         <div className="flex flex-col w-96 justify-between">
-          <h1 className="space-y-2 py-8 px-8 bg-white rounded-xl shadow-md text-4xl font-bold text-purple-600">Portfolio Tracker</h1>
+          <h1 className="space-y-2 py-8 px-8 bg-white rounded-xl shadow-md text-4xl font-bold text-purple-600">
+            Portfolio Tracker
+          </h1>
           <div className="space-y-2 py-8 px-8 bg-white rounded-xl shadow-md">
             <p className="text-lg text-gray-500">Welcome Back,</p>
             <h2 className="text-xl font-bold">{user.name}</h2>
           </div>
-          <AccountCard className="space-y-2 max-w-lg py-8 px-8 bg-white rounded-xl shadow-md" cryptoData={cryptoData} stockData={stockData} />
+          <AccountCard
+            className="space-y-2 max-w-lg py-8 px-8 bg-white rounded-xl shadow-md"
+            cryptoData={cryptoData}
+            stockData={stockData}
+          />
         </div>
 
-        <div className="w-full flex flex-row justify-around bg-white shadow-md ml-8 rounded-xl">
+        <div className="relative w-full flex flex-row justify-around bg-white shadow-md ml-8 rounded-xl">
+          <p className="text-6xl font-bold absolute p-10 left-0 top-0 ">
+            ${(totaler(currencies) + Number(VGRORate * vgro.quantity)).toFixed(2)}
+          </p>
           <Pie2
             type={`CRYPTO`}
             cryptoData={cryptoData}
@@ -81,5 +90,5 @@ export default function Pie() {
         </div>
       </div>
     </div>
-  );
+  )
 }
